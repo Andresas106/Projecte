4b8a3f42c1b3333c506457bc62ab5e0c9faa5f55
Andreu Verdes <averdes12@gmail.com>
2021-05-21 23:16:09 +0200

    Schema de BD casi acabat

--- DataBase_Schema&Data/Projecte_Schema.sql
+++ DataBase_Schema&Data/Projecte_Schema.sql
@@ -24,12 +24,12 @@ primary key(codi)
 );
 
 create table if not exists LiniaEscandall(
-id Int,
+plat INT,
 num INT,
 quantitat INT,
 unitat INT,
 ingredient INT,
-primary key(id),
+constraint primary key(plat, num),
 constraint FK_LINIA_UNITAT foreign key(unitat) references Unitat(codi),
 constraint FK_LINIA_INGREDIENT foreign key(ingredient) references Ingredient(codi)
 );
@@ -46,4 +46,35 @@ escandall INT,
 constraint FK_PLAT_LINIA foreign key(escandall) references LiniaEscandall(id),
 primary key(codi),
 constraint FK_PLAT_CATEGORIA foreign key(categoria) references Categoria(codi)
+);
+
+create table if not exists Cambrer(
+codi INT,
+nom varchar(30),
+cognom1 varchar(30),
+cognom2 varchar(30),
+usuari varchar(30),
+passw varchar(30),
+constraint primary key(codi)
+);
+
+create table if not exists Comanda (
+codi INT,
+data Date,
+cambrer INT,
+constraint primary key(codi),
+constraint FK_COMANDA_CAMBRER foreign key(cambrer) references Cambrer(codi)
+);
+
+create table if not exists LiniaComanda(
+comanda INT,
+num INT,
+quantitat int,
+constraint primary key(comanda, num),
+constraint FK_LINIACOMANDA_COMANDA foreign key(comanda) references Comanda(codi)
+);
+
+create table if not exists Taula(
+numero INT,
+constraint primary key(numero)
 );
