e6d117718c20aaff0d512acb7356f7aaba683344
Andreu Verdes <averdes12@gmail.com>
2021-05-27 02:10:09 +0200

    Comencament UWP

---
+++ UWP/GestioCartaCuina/MySQL_BD/CategoriaDB.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace MySQL_BD
+{
+    public class CategoriaDB
+    {
+
+        private int codi;
+        private String nom;
+        private String color;
+        public CategoriaDB() { }
+
+        public CategoriaDB(int codi, String nom, String color)
+        {
+            this.Codi = codi;
+            this.Nom = nom;
+            this.Color = color;
+        }
+
+        public int Codi { get => codi; set => codi = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Color { get => color; set => color = value; }
+
+        public static List<CategoriaDB> getCategories()
+        {
+            try
+            {
+                using (DBMySQL context = new DBMySQL())
+                {
+                    using (var connexio = context.Database.GetDbConnection())
+                    {
+                        connexio.Open();
+
+                        using (var consulta = connexio.CreateCommand())
+                        {
+                            //definir la consulta de teams
+                            consulta.CommandText = "select codi, nom, color from categoria";
+
+                            DbDataReader reader = consulta.ExecuteReader();
+
+                            List<CategoriaDB> categories = new List<CategoriaDB>();
+                            
+                            while (reader.Read())
+                            {
+                                int codi = reader.GetInt32(reader.GetOrdinal("codi"));
+                                String nom = reader.GetString(reader.GetOrdinal("nom"));
+                                String color = reader.GetString(reader.GetOrdinal("color"));
+
+
+                                CategoriaDB c = new CategoriaDB(codi, nom, color);
+                                categories.Add(c);
+                            }
+                            return categories;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return null;
+        }
+    }
+}
---
+++ UWP/GestioCartaCuina/MySQL_BD/DBMySQL.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySQL_BD
+{
+    public class DBMySQL : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("Server=localhost;Database=practica;UID=root;Password=");
+        }
+    }
+}
